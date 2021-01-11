cb86e94c933c036887bf6b773e26bdb30c810755
Yitian Hao <yh255@attu3.cs.washington.edu>
2021-01-10 22:38:37 -0800

    change design in open file

--- kernel/file.c
+++ kernel/file.c
@@ -49,28 +49,14 @@ int file_open(char *path, int mode)
   struct finfo *file;
   for (file = ftable.finfo; file < &ftable.finfo[NFILE]; file++)
   {
-    if (file->ip == ip)
+    if (file->ref_ct == 0)
     {
       file->access_permi = mode;
+      file->ip = ip;
       file->ref_ct++;
       // what is offset???
       process->fds[fd] = file;
       break;
-    }
-  }
-  if (process->fds[fd] == NULL)
-  {
-    for (file = ftable.finfo; file < &ftable.finfo[NFILE]; file++)
-    {
-      if (file->ref_ct == 0)
-      {
-        file->access_permi = mode;
-        file->ip = ip;
-        file->ref_ct++;
-        // what is offset???
-        process->fds[fd] = file;
-        break;
-      }
     }
   }
 
@@ -124,12 +110,14 @@ int file_dup(int fd)
   return new_fd;
 }
 
-int file_read(int fd, char* dst, uint n) {
+int file_read(int fd, char *dst, uint n)
+{
   // get the curr process
   struct proc *process = myproc();
 
   struct finfo *file = process->fds[fd];
-  if (file == NULL || (file->access_permi != O_RDONLY && file->access_permi != O_RDWR)) {
+  if (file == NULL || (file->access_permi != O_RDONLY && file->access_permi != O_RDWR))
+  {
     return -1;
   }
 
@@ -138,43 +126,49 @@ int file_read(int fd, char* dst, uint n) {
   // get curr offset
   uint offset = file->offset;
   int read = concurrent_readi(ip, dst, offset, n);
-  if (read == -1) {
+  if (read == -1)
+  {
     return -1;
   }
   file->offset = file->offset + read;
   return read;
 }
 
-int file_write(int fd, char* src, uint n) {
+int file_write(int fd, char *src, uint n)
+{
   // get the curr process
   struct proc *process = myproc();
 
   struct finfo *file = process->fds[fd];
-  if (file == NULL || (file->access_permi != O_WRONLY && file->access_permi != O_RDWR)) {
+  if (file == NULL || (file->access_permi != O_WRONLY && file->access_permi != O_RDWR))
+  {
     return -1;
   }
-  
+
   // get the file inode
   struct inode *ip = file->ip;
   // get curr offset
   uint offset = file->offset;
   int written = concurrent_writei(ip, src, offset, n);
-  if (written == -1) {
+  if (written == -1)
+  {
     return -1;
   }
   file->offset = file->offset + written;
   return written;
 }
 
-int file_stat(int fd, struct stat *st) {
-    // get the curr process
+int file_stat(int fd, struct stat *st)
+{
+  // get the curr process
   struct proc *process = myproc();
 
   struct finfo *file = process->fds[fd];
-  if (file == NULL) {
+  if (file == NULL)
+  {
     return -1;
   }
-  
+
   // get the file inode
   struct inode *ip = file->ip;
   concurrent_stati(ip, st);
